e07ed887b561c7fd6e89c91a7cdac053f6502455
Nada Ayman <nadaayman0662@gmail.com>
2026-05-03 22:14:38 +0300

    update complaint model

--- models/complaintModel.js
+++ models/complaintModel.js
@@ -3,14 +3,23 @@ const validator = require("validator");
 
 const ComplaintSchema = new mongoose.Schema(
   {
+    id:{
+      type:String,
+      trim:true,
+      require:[true, "Id is required"],
+      unique:[true,"Id must be unique"],
+      maxlength:[15,"User name must be at most 15 characters"]
+    },
     name: {
       type: String,
+      trim:true,
       required: [true, "User name is required"],
       minlength: [3, "User name must be at least 3 characters"],
       maxlength: [20, "User name must be at most 20 characters"],
     },
     email: {
       type: String,
+      trim:true,
       required: [true, "Email is required"],
 
       validate: {
@@ -22,32 +31,45 @@ const ComplaintSchema = new mongoose.Schema(
     },
     subject: {
       type: String,
+      trim:true,
       required: [true, "Subject is required"],
+       maxlength: [20, "subject must be at most 20 characters"]
     },
     service: {
       type: String,
+      trim:true,
       required: [true, "Service is required"],
+      maxlength: [20, "Service must be at most 20 characters"],
+      enum:['Delivery','Food Quality','Payment','Issue','App Bug','Other']
     },
     message: {
       type: String,
+      trim:true,
       required: [true, "Message content is required"],
       maxlength: [500, "Message must be at most 500 characters"],
     },
     status: {
       type: String,
+      trim:true,
       enum: ["pending", "in process", "resolved"],
       default: "pending",
+    },
+    adminResponse:{
+      type:String,
+      trim:true,
+      maxlength: [20, "Response message must be at most 500 characters"],
+      default:""
+      
     },
     userId: {
       type: mongoose.Schema.ObjectId,
       ref: "User",
-      required: true,
+      // required: true,
     },
   },
   {
     timestamps: true,
-  },
-);
+  },{ _id: false });
 
 const ComplaintModel = mongoose.model("Complaint", ComplaintSchema);
 module.exports = ComplaintModel;
